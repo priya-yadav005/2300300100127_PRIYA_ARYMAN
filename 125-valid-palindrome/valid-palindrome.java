29382222a05c5d682054f56f7783f5bfc0162472
Priya <priyayadav0375@gmail.com>
2026-08-16 02:29:32 +0530

    Time: 2 ms (99.33%) | Memory: 43.9 MB (96.35%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            // Left side ka space/symbol skip karo
+            if (!Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+                continue;
+            }
+
+            // Right side ka space/symbol skip karo
+            if (!Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+                continue;
+            }
+
+            // Dono valid characters ko compare karo
+            if (Character.toLowerCase(s.charAt(left)) !=
+                Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
