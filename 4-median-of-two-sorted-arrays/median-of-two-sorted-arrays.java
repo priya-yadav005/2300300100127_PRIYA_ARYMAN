40030bcfe545762c2b718d6ff93721c96fee5183
Priya <priyayadav0375@gmail.com>
2026-09-13 23:58:51 +0530

    Time: 2 ms (45.90%) | Memory: 48.7 MB (86.20%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,42 +1,26 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
-        // Always binary search on smaller array
-        if (nums1.length > nums2.length)
-            return findMedianSortedArrays(nums2, nums1);
+        int[] arr = new int[nums1.length + nums2.length];
 
-        int m = nums1.length;
-        int n = nums2.length;
+        int i = 0, j = 0, k = 0;
 
-        int low = 0;
-        int high = m;
-
-        while (low <= high) {
-
-            int cut1 = (low + high) / 2;
-            int cut2 = (m + n + 1) / 2 - cut1;
-
-            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
-            int r1 = (cut1 == m) ? Integer.MAX_VALUE : nums1[cut1];
-
-            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
-            int r2 = (cut2 == n) ? Integer.MAX_VALUE : nums2[cut2];
-
-            if (l1 <= r2 && l2 <= r1) {
-
-                if ((m + n) % 2 == 0) {
-                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
-                } else {
-                    return Math.max(l1, l2);
-                }
-
-            } else if (l1 > r2) {
-                high = cut1 - 1;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                arr[k++] = nums1[i++];
             } else {
-                low = cut1 + 1;
+                arr[k++] = nums2[j++];
             }
         }
 
-        return 0.0;
+        while (i < nums1.length) arr[k++] = nums1[i++];
+        while (j < nums2.length) arr[k++] = nums2[j++];
+
+        int n = arr.length;
+
+        if (n % 2 == 1)
+            return arr[n / 2];
+
+        return (arr[n/2 - 1] + arr[n/2]) / 2.0;
     }
 }
